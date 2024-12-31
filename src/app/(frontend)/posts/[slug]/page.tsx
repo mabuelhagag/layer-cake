1425946132096cc4364f092511821feee9de6a9b
Mohamad A <mohammed.abuelhagag@gmail.com>
2024-12-31 21:53:44 +0200

    conditionally render main image in post page component

--- src/app/(frontend)/posts/[slug]/page.tsx
+++ src/app/(frontend)/posts/[slug]/page.tsx
@@ -21,18 +21,20 @@ export default async function Page({
 
   return (
     <main className="container mx-auto grid grid-cols-1 gap-6 p-12">
-      <Image
-        className="w-full aspect-[800/300]"
-        src={urlFor(post.mainImage)
-          .width(800)
-          .height(300)
-          .quality(80)
-          .auto("format")
-          .url()}
-        alt={post?.mainImage?.alt || ""}
-        width={800}
-        height={300}
-      />
+      {post.mainImage && (
+        <Image
+          className="w-full aspect-[800/300]"
+          src={urlFor(post.mainImage)
+            .width(800)
+            .height(300)
+            .quality(80)
+            .auto("format")
+            .url()}
+          alt={post?.mainImage?.alt || ""}
+          width={800}
+          height={300}
+        />
+      )}
       <h1 className="text-4xl font-bold text-balance">{post?.title}</h1>
       <hr />
       <Link href="/posts">&larr; Return to index</Link>
